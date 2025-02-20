fb4dc04574043bdb6a3a57c2820af56305041d15
Amogh Sharma <amoghsharma02@gmail.com>
2025-02-20 11:52:12 +1100

    Initial Commit

---
+++ PizzaTime/PizzaTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaTimeApp.swift
+//  PizzaTime
+//
+//  Created by Amogh Sharma  on 20/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct PizzaTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
